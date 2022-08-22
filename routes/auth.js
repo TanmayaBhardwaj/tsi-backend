909335b0d2c2247a961a94a96c367b430387ccd2
TanmayaBhardwaj <89739357+TanmayaBhardwaj@users.noreply.github.com>
2022-08-23 00:28:12 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const { Enquiry } = require("../Enquiry");
+const { Home } = require("../Home");
+
 const express = require("express");
 // const {thanku}
 const router = express.Router();
-
+router.get("/", Home);
 router.post("/enquiry", Enquiry);
 // router.get("/thanku",thanku)
 
